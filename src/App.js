3c0aad67475834e8152724730092ed5c5c21c340
Nathan Dunn <ndunn@me.com>
2020-12-04 16:53:48 -0800

    compiles down

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import QueryString from 'querystring'
 import PureComponent from './components/PureComponent'
 import ReactGA from 'react-ga'
 import {Helmet} from 'react-helmet'
-import {ApplicationWrapper} from './components/ApplicationWrapper'
+// import {ApplicationWrapper} from './components/ApplicationWrapper'
 
 function initializeReactGA() {
   ReactGA.initialize('UA-136203053-1')
@@ -13,49 +13,46 @@ function initializeReactGA() {
 
 export class App extends PureComponent {
 
-  render(){
-    return <div>asdfasdf</div>
-  }
 
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
+
+    const urlVariables = QueryString.parse(location.hash.substr(1))
+    console.log('url variables', urlVariables)
+    this.state = {
+      wizard: urlVariables.wizard ? urlVariables.wizard : undefined
+    }
+  }
   //
-  //   const urlVariables = QueryString.parse(location.hash.substr(1))
-  //   console.log('url variables', urlVariables)
-  //   this.state = {
-  //     wizard: urlVariables.wizard ? urlVariables.wizard : undefined
-  //   }
-  // }
   //
-  //
-  // render() {
-  //
-  //   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  //     // eslint-disable-next-line no-console
-  //     console.log('Xena Gene Set Viewer Dev environment')
-  //   } else {
-  //     // production code
-  //     // eslint-disable-next-line no-console
-  //     console.log('Xena Gene Set Viewer Initialized')
-  //     initializeReactGA()
-  //   }
-  //
-  //   return (
-  //     <div>
-  //       <Helmet
-  //         link={[
-  //           {
-  //             'rel': 'icon',
-  //             'type': 'image/png',
-  //             'href': 'https://raw.githubusercontent.com/ucscXena/XenaGoWidget/develop/src/images/xenalogo_hfz_icon.ico'
-  //           }
-  //         ]}
-  //         meta={[
-  //           {name: 'description', content: 'Xena Gene Set Viewer'}
-  //         ]}
-  //         title="Xena Gene Set Comparison"
-  //       />
-  //       <ApplicationWrapper/>
-  //     </div>)
-  // }
+  render() {
+
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      console.log('Xena Gene Set Viewer Dev environment')
+    } else {
+      // production code
+      // eslint-disable-next-line no-console
+      console.log('Xena Gene Set Viewer Initialized')
+      initializeReactGA()
+    }
+
+    return (
+      <div>
+        <Helmet
+          link={[
+            {
+              'rel': 'icon',
+              'type': 'image/png',
+              'href': 'https://raw.githubusercontent.com/ucscXena/XenaGoWidget/develop/src/images/xenalogo_hfz_icon.ico'
+            }
+          ]}
+          meta={[
+            {name: 'description', content: 'Xena Gene Set Viewer'}
+          ]}
+          title="Xena Gene Set Comparison"
+        />
+        {/*<ApplicationWrapper/>*/}
+      </div>)
+  }
 }
